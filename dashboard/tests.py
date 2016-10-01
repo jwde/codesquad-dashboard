9d2fcf9e16734af013a19bd1550a73adef437d86
jwde <jaydestories@gmail.com>
2016-09-30 23:01:19 -0400

    added test that always passes and circle config

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+# example mock / test case
+class AlwaysPassThisTest(TestCase):
+    def setUp(self):
+        # set up mock model
+        #(modelname).objects.create(field1="value1", field2="value2")
+        pass
+
+    # tests must start with 'test_'
+    def test_always_passes(self):
+        self.assertEqual(True, True)
