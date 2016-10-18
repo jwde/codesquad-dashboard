1073fb1fc9822f0e37d9f3c6e9d4eaecfcdc0827
Alli Pine <allipine@resnet161-224.medford.tufts.edu>
2016-10-18 16:06:50 -0400

    Added course class and allow many-many relationship

--- dashboard/models.py
+++ dashboard/models.py
@@ -16,7 +16,19 @@ class Student(User):
 		choices=PRIVACY_CHOICES,
 		default=PRIVATE,
 	)
-	course = models.CharField(max_length=200)
 
+	def __str__(self):
+		return self.username
 
+class Course(models.Model):
+	name = models.CharField(max_length=200)
+	enrolled_students = models.ManyToManyField(Student, through='Enrollment')
+
+	def __str__(self):
+		return self.name
+
+class Enrollment(models.Model):
+	student = models.ForeignKey(Student, on_delete=models.CASCADE)
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
+	start_date = models.DateField()
 
--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
-from models import Student
+from models import Student, Course, Enrollment
 
 
 class StudentTestCase(TestCase):
     def setUp(self):
-        Student.objects.create(username="horsecrzy85",password="i<3horses",first_name="Droolia",
-                               last_name="Boyer", email="dboyer@gmail.com",course="Basics of HTML/CSS")
+        dboyer = Student.objects.create(username="horsecrzy85",password="i<3horses",first_name="Droolia",
+                               last_name="Boyer", email="dboyer@gmail.com")
+        html = Course.objects.create(name="Intro to CSS/HTML")
+        e1 = Enrollment(student=dboyer,course=html,start_date="2016-10-10")
+        e1.save()
 
     def test_students(self):
         """ Just tests if it was created in the database, the default is private, and can be updated """
@@ -15,6 +18,11 @@ class StudentTestCase(TestCase):
         droolia.privacy_setting = "PU"
         droolia.save()
         self.assertEqual(droolia.privacy_setting, "PU")
+
+    def test_enrollment(self):
+        html = Course.objects.get(name="Intro to CSS/HTML")
+        dboyer = Student.objects.get(first_name="Droolia")
+        self.assertEqual(html.enrolled_students.get(pk=dboyer.pk), dboyer)
 
 # example mock / test case
 class AlwaysPassThisTest(TestCase):
