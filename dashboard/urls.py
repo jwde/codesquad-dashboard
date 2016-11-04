312a1ac47116e46823a5de2eb488a27609494668
Julia Moyer <julia.moyer@tufts.edu>
2016-11-04 15:25:48 -0400

    added built-in login endpoints

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,23 @@
 from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url('^', include('django.contrib.auth.urls')),
 ]
+
+# ============================================================
+# django.contrib.auth.urls includes:
+# 
+# ^login/$ [name='login']
+# ^logout/$ [name='logout']
+# ^password_change/$ [name='password_change']
+# ^password_change/done/$ [name='password_change_done']
+# ^password_reset/$ [name='password_reset']
+# ^password_reset/done/$ [name='password_reset_done']
+# ^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$ [name='password_reset_confirm']
+# ^reset/done/$ [name='password_reset_complete']
+# 
+# https://docs.djangoproject.com/en/1.9/topics/auth/default/#module-django.contrib.auth.views
