73b1956f292bac048cdfb1a9219dc25827e63af3
mdettelson <mdettelson@gmail.com>
2016-11-05 16:16:02 -0400

    added revised urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,19 +5,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url('^', include('django.contrib.auth.urls')),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^password_change/$', auth_views.password_change, name='password_change'),
+    url(r'^password_change/done/$$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
-
-# ============================================================
-# django.contrib.auth.urls includes:
-# 
-# ^login/$ [name='login']
-# ^logout/$ [name='logout']
-# ^password_change/$ [name='password_change']
-# ^password_change/done/$ [name='password_change_done']
-# ^password_reset/$ [name='password_reset']
-# ^password_reset/done/$ [name='password_reset_done']
-# ^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$ [name='password_reset_confirm']
-# ^reset/done/$ [name='password_reset_complete']
-# 
-# https://docs.djangoproject.com/en/1.9/topics/auth/default/#module-django.contrib.auth.views
