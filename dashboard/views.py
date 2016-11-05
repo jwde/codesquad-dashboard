670c865a658cd8110b11b39a63466e983232eb3f
Julia Moyer <julia.moyer@tufts.edu>
2016-11-05 17:54:32 -0400

    putting dashboard at index

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("hello world")
+def dashboard(request):
+    # return HttpResponse("hello world")
+    return render(request, "dashboard/templates/student_dashboard.html", {})
+
