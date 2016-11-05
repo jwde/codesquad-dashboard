9c7993e4b68f57998a13bffdc1f006166ee4436c
Julia Moyer <julia.moyer@tufts.edu>
2016-11-05 18:04:55 -0400

    fiddling w things

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 # Create your views here.
-@login_required(login_url='login/')
+# @login_required(login_url='login/')
 def dashboard(request):
     # return HttpResponse("hello world")
-    return render(request, "dashboard/templates/student_dashboard.html", {})
+    return render(request, "/dashboard/templates/student_dashboard.html", {})
 
