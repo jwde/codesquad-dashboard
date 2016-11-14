8e181db92b32e98a8b0129172bf08157730943c8
Julia Moyer <julia.moyer@tufts.edu>
2016-11-13 20:13:57 -0500

    just stuffs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,11 +9,12 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html',\
                                         'authentication_form': LoginForm},\
         name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^password_change/$', auth_views.password_change, name='password_change'),
-    url(r'^password_change/done/$$', auth_views.password_change_done, name='password_change_done'),
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 
-            auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html'},\
+        name='logout'),
+    # url(r'^password_change/$', auth_views.password_change, name='password_change'),
+    # url(r'^password_change/done/$$', auth_views.password_change_done, name='password_change_done'),
+    # url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    # url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 
+    #         auth_views.password_reset_confirm, name='password_reset_confirm'),
+    # url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
